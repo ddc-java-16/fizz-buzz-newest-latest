2bece1f7977ea58e4b7e376a4416cf87d8710088
che money <cmelendez15@cnm.edu>
2023-09-21 16:10:10 -0500

    added constants and tests

--- src/main/java/edu/cnm/deepdive/fizzbuzz/Main.java
+++ src/main/java/edu/cnm/deepdive/fizzbuzz/Main.java
@@ -2,13 +2,16 @@ package edu.cnm.deepdive.fizzbuzz;
 
 public class Main {
 
+  private static final String FIZZ_RESULT = "fizz";
+  private static final String BUZZ_RESULT = "buzz";
+  private static final String FIZZ_BUZZ_RESULT = FIZZ_RESULT + BUZZ_RESULT;
   public static void main(String[] args) {
     for (int i = 1; i <= 100; i++) {
       System.out.println(detectFizzBuzz(i));
     }
   }
 
-  private static String detectFizzBuzz(int i) {
+  static String detectFizzBuzz(int i) {
     int remainder = i % 15;
     return switch (remainder) {
       case 0 -> ("fizzbuzz");
