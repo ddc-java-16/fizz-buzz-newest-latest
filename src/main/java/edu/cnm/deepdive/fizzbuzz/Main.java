6a078a0567184cb90fc6a410c220d20ad19ae5ca
che money <cmelendez15@cnm.edu>
2023-10-10 10:10:00 -0500

    many tests

--- src/main/java/edu/cnm/deepdive/fizzbuzz/Main.java
+++ src/main/java/edu/cnm/deepdive/fizzbuzz/Main.java
@@ -1,5 +1,8 @@
 package edu.cnm.deepdive.fizzbuzz;
 
+import java.util.EnumSet;
+import java.util.Set;
+
 public class Main {
 
   private static final String FIZZ_RESULT = "fizz";
@@ -7,19 +10,25 @@ public class Main {
   private static final String FIZZ_BUZZ_RESULT = FIZZ_RESULT + BUZZ_RESULT;
   public static void main(String[] args) {
     for (int i = 1; i <= 100; i++) {
-      System.out.println(detectFizzBuzz(i));
+      Set<FizzBuzz> value = FizzBuzz.valueOf(i);
+      System.out.println(value.isEmpty() ? i : value);
     }
   }
 
-  static String detectFizzBuzz(int i) {
-    int remainder = i % 15;
-    return switch (remainder) {
-      case 0 -> ("fizzbuzz");
-      case 3, 6, 9, 12 -> ("fizz");
-      case 5, 10 -> "buzz";
-      default -> String.valueOf(i);
-    };
-  }
 
+public enum FizzBuzz {
+    FIZZ,
+    BUZZ;
+  public static Set<FizzBuzz> valueOf(int value) {
+   Set<FizzBuzz> result = EnumSet.noneOf(FizzBuzz.class);
+       if(value % 3 == 0) {
+         result.add(FIZZ);
 
-}
+       }
+       if (value % 5 == 0) {
+         result.add(BUZZ);
+       }
+       return result;
+   }
+
+}}
--- src/test/java/edu/cnm/deepdive/fizzbuzz/MainTest.java
+++ src/test/java/edu/cnm/deepdive/fizzbuzz/MainTest.java
@@ -2,19 +2,47 @@ package edu.cnm.deepdive.fizzbuzz;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import edu.cnm.deepdive.fizzbuzz.Main.FizzBuzz;
+import java.util.EnumSet;
+import java.util.Set;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class MainTest {
 
-  @ParameterizedTest
-  void main() {
+
+  static class FizzBuzzTest {
+
+    @DisplayName(" Testing expected FIZZ:")
+    @ParameterizedTest(name = "[{index}] Assert that FizzBuzz.valueOf({0}) returns [FizzBuzz.FIZZ].")
+    @ValueSource(ints = {3, 36, 9, 12})
+    void valueOf(int value) {
+      assertEquals(EnumSet.of(FizzBuzz.FIZZ), Main.FizzBuzz.valueOf(value));
+    }
   }
 
-  @ParameterizedTest
-  @CsvFileSource(resources = "fizz-buzz-cases.csv", useHeadersInDisplayName = true)
-  void detectFizzBuzz(int input, String expected) {
-    assertEquals(expected, Main.detectFizzBuzz(input));
+  @DisplayName("Testing expected BUZZ:")
+  @ParameterizedTest(name = "[{index}] Assert that FizzBuzz.valueOf({0}) returns [FizzBuzz.FIZZ].")
+  @ValueSource(ints = {5, 10, 50, 1_555_555_555})
+  void valueOf_fizz(int value) {
+    assertEquals(EnumSet.of(FizzBuzz.BUZZ), Main.FizzBuzz.valueOf(value));
+  }
+
+
+  @DisplayName("Testing expected FIZZBUZZ:")
+  @ParameterizedTest(name = "[{index}] Assert that FizzBuzz.valueOf({0}) returns [FizzBuzz.FIZZ].")
+  @ValueSource(ints = {15, 30, 75})
+  void valueOf_fizzBuzz(int value) {
+    assertEquals(EnumSet.allOf(FizzBuzz.class), Main.FizzBuzz.valueOf(value));
+  }
+
+  @DisplayName("Testing expected neither:")
+  @ParameterizedTest(name = "[{index}] Assert that FizzBuzz.valueOf({0}) returns [FizzBuzz.FIZZ].")
+  @ValueSource(ints = {117, 34})
+  void valueOf_none(int value) {
+    assertEquals(EnumSet.noneOf(FizzBuzz.class), Main.FizzBuzz.valueOf(value));
   }
 }
